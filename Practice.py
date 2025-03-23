82c7cbc5b6c4490044e4d6e3b539d3c90e84ae6f
Zach Schultz <Zach.Schultz.2.0.1@gmai.com>
2025-03-23 13:11:43 -0400

    init Commit

---
+++ Practice.py
@@ -0,0 +1,8 @@
+# Written by Zachary Schultz
+print("This is the HGV Learning Git Repo")
+print("Created by Zachary Schultz")
+print("Your goal here to complete The tutorial is to add anything to this file and commit it to GitHub")
+print("It can be a simple as printing your name or as complex as some graphical programming")
+print("The only requement that it doesn't break the other code already made and that it has your name it in somewhere to show that you made it")
+
+# Written by ____ 
